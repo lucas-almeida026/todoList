e8c55b6ce7c28d8654b486dd13e8d268f71cfae6
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2020-09-24 11:55:10 -0300

    v1.2
    feature added: check box toggle

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 const encode = id => (id * 7).toString(16) + (id * 140).toString(16).substring(0, 2)
 const decode = encodedId => (parseInt('0x' + encodedId.substring(0, encodedId.length - 2)) / 7).toString()
 
-const getTaskById = taskId => {
-    console.log(taskId)
+const getTaskById = taskId => localStorage.getItem('tasks').split(';').map(e => JSON.parse(e)).filter(e => e.id == taskId)[0]
+const updateTasks = (task, change) => {
+    const tasks = localStorage.getItem('tasks')
+    const lefting = tasks.split(';').map(e => JSON.parse(e)).filter(e => e.id < task.id)
+    const righting = tasks.split(';').map(e => JSON.parse(e)).filter(e => e.id > task.id)
+    change(task)
+    localStorage.setItem('tasks', [...lefting, task, ...righting].map(e => JSON.stringify(e)).join(';'))
 }
 
 const deleteTask = taskId => {
@@ -15,7 +20,9 @@ const editTask = taskId => {
 
 const checkTask = taskId => {
     decodedId = decode(taskId.substring(taskId.indexOf('_') + 1, taskId.length))
-    getTaskById(decodedId)
+    const thisTask = getTaskById(decodedId)
+    thisTask.finished ? updateTasks(thisTask, t =>{t.finished = false}) : updateTasks(thisTask, t => {t.finished = true})
+    renderTasks(localStorage.getItem('tasks')) 
 }
 
 const createTaskElements = tasks => {
@@ -28,22 +35,28 @@ const createTaskElements = tasks => {
         let btnDelete = document.createElement('div')
         btnEdit.classList.add('option')        
         btnEdit.id = 'edit_' + encode(e.id)
-        btnEdit.innerHTML = '<i class="material-icons materialOptions">edit</i>'
+        btnEdit.innerHTML = '<i class="material-icons materialOptions '+ localStorage.getItem('colorTheme') +'Material">edit</i>'
         btnEdit.onclick = e => editTask(e.target.tagName == 'I' ? e.target.parentNode.id : e.target.id)
+
         btnDelete.classList.add('option')
         btnDelete.id = 'delete_' + encode(e.id)
         btnDelete.innerHTML = '<i class="material-icons materialOptions">delete</i>'
         btnDelete.onclick = e => deleteTask(e.target.tagName == 'I' ? e.target.parentNode.id : e.target.id)
+        
         options.classList.add('options')
         options.append(btnEdit)
         options.append(btnDelete)
+
         inputTextName.type = 'text'
         inputTextName.value = e.name
         inputTextName.readOnly = true
+
         checkBox.id = 'chk_' + encode(e.id)
         checkBox.classList.add('checkBox')
-        checkBox.onclick = e => checkTask(e.target.id)
+        e.finished ? checkBox.innerHTML = '<i class="material-icons materialOptions">check_box</i>' : checkBox.innerHTML = '<i class="material-icons materialOptions">check_box_outline_blank</i>'
         e.finished ? checkBox.classList.add('checked') : false
+        checkBox.onclick = () => checkTask(checkBox.id)
+
         task.classList.add('task')
         task.id = 'task_' + encode(e.id)
         task.append(checkBox)
@@ -91,6 +104,7 @@ const renderTasks = async tasks => {
             list.append(el)
         }
     }
+    renderColorTheme(localStorage.getItem('colorTheme'))
 }
 
 const renderColorTheme = theme => {
@@ -98,6 +112,7 @@ const renderColorTheme = theme => {
     const header = document.getElementsByTagName('header')[0]
     const optionButtons = document.getElementsByClassName('option')
     const menuItems = document.getElementsByClassName('menuItem')
+    const checkBoxes = document.getElementsByClassName('checkBox')
     body.classList.add(theme + 'Bg')
     header.classList.add(theme + 'Bg')
     for(btn of optionButtons){
@@ -105,9 +120,15 @@ const renderColorTheme = theme => {
         thisBtn.classList.add(theme + 'Material')
         !!thisBtn.classList.item(2) ? thisBtn.classList.remove(thisBtn.classList.item(1)) : false
     }
+
     for(btn of menuItems){
         btn.classList.item(1) ? btn.classList.remove(btn.classList.item(1)) : false
         btn.id.substring(6, btn.id.length).toLowerCase() == theme ? btn.classList.add('selectedTheme') : false
+    }
+    
+    for(chk of checkBoxes){
+        !!chk.children[0].classList.item(2) ? chk.children[0].classList.remove(chk.children[0].classList.item(2)) : false
+        chk.children[0].classList.add(theme + 'Material')
     } 
     !!body.classList.item(1) ? body.classList.remove(body.classList.item(0)) : false
     !!header.classList.item(1) ? header.classList.remove(header.classList.item(0)) : false
